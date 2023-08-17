c4f9d478f35d0e55c9c786e5e3e17c3946ee5e64
Felicitas <felicitas.aguerralde@gmail.com>
2023-08-17 19:35:34 -0300

    Ejercicio 1 andando

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-       Persona Felicitas= new Persona(34206598);
-       System.out.println("La persona se encuentra "+Felicitas.getEstado(19));
-       Felicitas.getCumpleanios("2023-07-16");
+       Persona Persona1= new Persona("Felicitas", "Aguerralde",34206598);
+       Persona1.setPeso(55.00);
+       Persona1.setAltura(1.71);
+       Persona1.setSexo('F');
+       Persona1.getEstado();
+       Persona1.setFechaNacimiento(LocalDate.of(1989,3,21));
+       Persona1.setEdad(34);
+       Persona1.getCumpleAnios();
+       Persona1.getVotar();
+       Persona1.getMayorEdad();
+       Persona1.validarEdad();
+       Persona1.getPersona();
         }
     }
--- src/Persona.java
+++ src/Persona.java
@@ -1,16 +1,36 @@
-import java.util.Calendar;
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Persona {
     //--------------Atributos---------------
-    String nombre="N";
-    String apellido="N";
-    int edad;
-    String fecha_nacimiento="2000-01-01";
-    int dni;
-    String sexo= "F";
-    double peso=1;
-    double altura=1;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private LocalDate fecha_nacimiento;
+    private int dni;
+    private char sexo;
+    private double peso;
+    private double altura;
+    //constantes
+    public static final int MAYOREDAD=18;
+    public static final int POTENCIA=2;
+    public static final double MIN=18.5;
+    public static final int VOTA=16;
+
+    public static final double MAX=25;
+
 
     //--------------Contructores--------------
+    public Persona(){
+        fecha_nacimiento= LocalDate.of(2000,01,01); //valores por defecto
+        sexo='F';
+        nombre="N";
+        apellido="N";
+        peso=1;
+        altura=1;
+    }
+    //preguntar para que tengo que crear estos constructores
+
     public Persona(int dni) {
         this.dni = dni;
     }
@@ -19,76 +39,134 @@ public class Persona {
         this.apellido = apellido;
         this.dni = dni;
     }
-    public Persona(String nombre, String apellido, String fecha_nacimiento, int dni) {
+    public Persona(String nombre, String apellido, LocalDate fecha_nacimiento, int dni) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.fecha_nacimiento = fecha_nacimiento;
         this.dni = dni;
     }
 
+
     //--------------Metodos----------------------
-public double getMasaCorporal(double peso, double altura){
-        double masa_corporal=0;
-        masa_corporal=peso/Math.pow(altura,2);
-        return masa_corporal;
-}
-public String getEstado(double masa_corporal){
-        String estado="en estado";
-        if(masa_corporal>18.5 && masa_corporal<25){
-            return estado;
-        }
-        else {
-            estado="fuera de estado";
-            return estado;
-        }
-}
+    //Get
 
-public void getCumpleanios(String fecha_nacimiento){
-    // Fecha actual
-    String [] cadena =getFechaActual();
-    //Paso los datos de string a int
-    int anio_actual=Integer.parseInt(cadena[0]);
-    int mes_actual=Integer.parseInt(cadena[1]);
-    int dia_actual=Integer.parseInt(cadena[2]);
-
-    //Separo los datos de la fecha
-    String[] cadena_nacimiento=fecha_nacimiento.split("-");
-
-    //Paso los datos de string a int
-    int anio_cumple=Integer.parseInt(cadena_nacimiento[0]);
-    int mes_cumple=Integer.parseInt(cadena_nacimiento[1]);
-    int dia_cumple=Integer.parseInt(cadena_nacimiento[2]);
-
-    //Indico si cumple o no
-    if(anio_actual==anio_cumple && mes_actual==mes_cumple && dia_actual==dia_cumple){
-        System.out.println("Hoy es el cumpleaños!!!");
+    public String getNombre(){
+        return this.nombre;
     }
-    else{
-        System.out.println("Hoy no es el cumpleaños");
+    public String getApellido(){
+        return this.apellido;
     }
-}
-public void getEsMayorEdad(String fecha_nacimiento){
-    String [] cadena=getFechaActual();
+    public int getEdad(){
+        return this.edad;
+    }
+    public LocalDate getFecha_nacimiento() {
+        return this.fecha_nacimiento;
+    }
+    public int getDni(){
+        return this.dni;
+    }
+    public char getSexo(){
+        return this.sexo;
+    }
+    public double getPeso(){
+        return this.peso;
+    }
+    public double getAltura(){
+        return this.altura;
+    }
 
-    //Paso los datos de string a int
-    int anio_cumple=Integer.parseInt(cadena[0]);
-    int mes_cumple=Integer.parseInt(cadena[1]);
-    int dia_cumple=Integer.parseInt(cadena[2]);
+    //Set
+    public String setNombre(String nuevoNombre){
+        return this.nombre=nuevoNombre;
+    }
+    public String setApellido(String nuevoApellido){
+        return this.apellido=nuevoApellido;
+    }
+    public int setEdad(int nuevaEdad){
+        return this.edad=nuevaEdad;
+    }
+    public LocalDate setFechaNacimiento(LocalDate nuevaFechaNacimiento){
+        return this.fecha_nacimiento=nuevaFechaNacimiento;
+    }
+    public int setDni(int nuevoDni){
+        return this.dni=nuevoDni;
+    }
+    public char setSexo(char nuevoSexo){
+        return this.sexo=nuevoSexo;
+    }
+    public double setPeso(double nuevoPeso){
+        return this.peso=nuevoPeso;
+    }
+    public double setAltura(double nuevaAltura){
+        return this.altura=nuevaAltura;
+    }
+
+    //Indice de masa corporal
+    public double getIMC(){
+        return  this.peso/(Math.pow(this.altura,POTENCIA));
+        }
+    //Estado
+    public void getEstado(){
+        if(this.getIMC()>=MIN && this.getIMC()<MAX){
+            System.out.println(this.getNombre()+ " se encuentra en estado");
+        }
+        else{
+            System.out.println(this.getNombre()+" se encuentra fuera de estado");
+        }
+    }
+    //Cumpleaños
+    public void getCumpleAnios(){
+        LocalDate fecha_actual = LocalDate.now();
+        if(fecha_actual.equals(this.fecha_nacimiento)){
+            System.out.println("Hoy cumple años!!!");
+        }
+        else{
+            System.out.println("Hoy no es su cumpleaños :(");
+        }
+    }
+    //Mayor de edad
+    public void getMayorEdad(){
+        LocalDate fecha_actual = LocalDate.now();
+        Period periodo = Period.between(this.fecha_nacimiento, fecha_actual);
+        if(periodo.getYears()>=MAYOREDAD){
+            System.out.println("Es mayor de edad");
+        }
+        else{
+            System.out.println("Es menor de edad");
+        }
+    }
+    //Puede votar
+    public void getVotar(){
+        LocalDate fecha_actual = LocalDate.now();
+        Period periodo = Period.between(this.fecha_nacimiento, fecha_actual);
+        if(periodo.getYears()>=VOTA){
+            System.out.println("Está habilitado para votar");
+        }
+        else{
+            System.out.println("No está habilitado para votar");
+        }
+    }
+    public void validarEdad(){
+        LocalDate fecha_actual = LocalDate.now();
+        Period periodo = Period.between(this.fecha_nacimiento, fecha_actual);
+        if(periodo.getYears()==this.edad){
+            System.out.println("La edad es correcta");
+        }
+        else{
+            System.out.println("La edad es incorrecta");
+        }
+    }
+
+    public void getPersona(){
+        System.out.println("Nombre y Apellido: "+this.nombre +" "+this.apellido +
+                        "\nEdad: "+this.edad+
+                        "\nFecha de nacimiento: "+this.fecha_nacimiento+
+                        "\nD.N.I: "+this.dni+
+                        "\nSexo: "+this.sexo+
+                        "\nPeso: "+this.peso+
+                        "\nAltura: "+this.altura
+                );
 
 
-}
-public String[] getFechaActual(){
-    // Fecha actual
-    Calendar calendar = Calendar.getInstance();
-
-    // Año:
-    int anio_actual=calendar.get(Calendar.YEAR);
-    // Mes:
-    int mes_actual=calendar.get(Calendar.MONTH);
-    // Dia:
-    int dia_actual=calendar.get(calendar.DAY_OF_MONTH);
-    //Separo los datos de la fecha
-    String[] cadena=fecha_nacimiento.split("-");
-    return cadena;
-}
+    }
 }
